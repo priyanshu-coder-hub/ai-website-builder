ea0baa11fda848575140418b8cb7ecdb1e981a5e
priyanshu-coder-hub <priyanshusultanpur09@gmail.com>
2026-04-28 07:29:54 +0530

    Update server URL to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
 
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://ai-website-builder-backend-41id.onrender.com"
 function App() {
   useGetCurrentUser()
   const {userData}=useSelector(state=>state.user)
